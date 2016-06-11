54b8a83ec602d53b0ed4b643f862eadacf152ad3
mrmroshan <mrmroshan@yahoo.com>
2016-06-11 12:38:56 +0000

    laravel 5 | Section 3 | Part 1 The built in Blade Templating Engine code changes

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul class="menuul">
+            <li class="menuli"><a href="#">Home</a></li>
+            <li class="menuli"><a href="#">Blog</a></li>
+            <li class="menuli"><a href="#">About</a></li>
+            <li class="menuli"><a href="#">Contact</a></li>
+            
+        </ul>
+    </nav>
+</header>
